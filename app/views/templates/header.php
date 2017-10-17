c055e6a90b069a69ebb63c2235e0c58f0afb803a
tcanham <tascanham@gmail.com>
2017-10-17 08:49:07 +0100

    17/10/2017

---
+++ app/views/templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tom Canham Web Developer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/jquery-ui.css">
+	<script src="assets/js/jquery-migrate-3.0.0.min.js "></script>
+        <script src="assets/js/jquery-ui.min.js"></script>
+	<script src="assets/js/script.js"></script>
+	<meta charset="UTF-8">
+</head>
+<body>
+    
